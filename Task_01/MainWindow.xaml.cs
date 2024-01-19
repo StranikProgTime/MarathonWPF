c50081038d9c4242dfcab7ef69c5839e58ff8aad
StranikProgTime <151746363+StranikProgTime@users.noreply.github.com>
2024-01-19 19:30:33 +0300

    Task_01

--- Task_01/MainWindow.xaml.cs
+++ Task_01/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Task_01
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You pushed this button");
+        }
     }
 }
